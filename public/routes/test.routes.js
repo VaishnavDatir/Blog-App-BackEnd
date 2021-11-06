fe6769f7a2bf3cb24fe9de1cb4da68d0191a9f38
Vaishnav Datir <vaishnav.datir@gmail.com>
2021-11-06 23:57:00 +0530

    Added JS build

--- public/routes/test.routes.js
+++ public/routes/test.routes.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.testRouter = void 0;
 const express_1 = __importDefault(require("express"));
-const error_exception_1 = __importDefault(require("../exceptions/error.exception"));
 const testRouter = (0, express_1.default)();
 exports.testRouter = testRouter;
 testRouter.get("/servercheck", (req, res, next) => {
@@ -13,8 +12,4 @@ testRouter.get("/servercheck", (req, res, next) => {
         success: true,
         message: "Server is working",
     });
-});
-testRouter.get("/checkMainError", (req, res, next) => {
-    const error = new error_exception_1.default(404, "This is error");
-    next(error);
 });
